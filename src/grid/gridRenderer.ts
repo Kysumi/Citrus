f56fe2f88b02408a652dc4728bddd18bcfa7522a
Maunder <scott.maunder@archipro.co.nz>
2021-08-11 19:12:17 +1200

    Chunking up tiles

--- src/grid/gridRenderer.ts
+++ src/grid/gridRenderer.ts
@@ -4,10 +4,23 @@ import { Vec2 } from "../vector/vec";
 let gridContainer: Container;
 
 const TILE_SIZE = 20;
+const CHUNK_SIZE = 10;
+
+const CHUNK_COUNT = 5;
+const CHUNK_WIDTH = CHUNK_SIZE * TILE_SIZE;
 
 interface GridConfig {
     dimensions: Vec2;
-    position: Vec2;
+    position?: Vec2;
+}
+
+function getChunkOffset(chunkPosition: Vec2): Vec2 {
+    const { x, y } = chunkPosition;
+
+    return {
+        x: (x * CHUNK_WIDTH) / 2,
+        y: (y * CHUNK_WIDTH) / 2,
+    };
 }
 
 export function renderGrid(dimensions: Vec2, app: Application): void {
@@ -15,22 +28,31 @@ export function renderGrid(dimensions: Vec2, app: Application): void {
         app.stage.removeChild(gridContainer);
     }
 
-    gridContainer = new Container();
+    for (let x = 0; x < CHUNK_COUNT; x++) {
+        for (let y = 0; y < CHUNK_COUNT; y++) {
+            gridContainer = new Container();
 
-    drawNewGrid({ dimensions, position: { x: 50, y: 50 } }, gridContainer);
+            const config = { dimensions };
+            drawChunk(config, gridContainer);
 
-    app.stage.addChild(gridContainer);
+            const { x: chunkX, y: chunkY } = getChunkOffset({ x, y });
+            gridContainer.x = chunkX + 50;
+            gridContainer.y = chunkY + 50;
+
+            app.stage.addChild(gridContainer);
+        }
+    }
 }
 
-function drawNewGrid(config: GridConfig, container: Container): void {
-    const { dimensions, position } = config;
+function drawChunk(config: GridConfig, container: Container): void {
+    const { dimensions } = config;
 
     for (let x = 0; x < dimensions.x; x++) {
         for (let y = 0; y < dimensions.y; y++) {
             const rectangle = new Graphics();
 
-            const xPos = position.x + x * TILE_SIZE;
-            const yPos = position.y + y * TILE_SIZE;
+            const xPos = x * TILE_SIZE;
+            const yPos = y * TILE_SIZE;
 
             rectangle.lineStyle(2, 0x999999);
             rectangle.beginFill(x % 2 ? 0xf0f8ff : 0xfff0f8);
