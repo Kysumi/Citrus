16b55565f0632e97adea652d8ddd5408ba55fa3a
Maunder <scott.maunder@archipro.co.nz>
2021-08-16 12:05:57 +1200

    Adding bitecs

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,48 @@ import { renderGrid } from "./grid/gridRenderer";
 import { loadSprites } from "./asset-loaders/spriteLoader";
 import { Actor } from "./actor/actor";
 import { Container } from "pixi.js";
+import { createWorld, Types, defineComponent, defineQuery, addEntity, addComponent, defineSystem, pipe } from "bitecs";
+
+const Vector3 = { x: Types.f32, y: Types.f32, z: Types.f32 };
+const Position = defineComponent(Vector3);
+const Velocity = defineComponent(Vector3);
+
+const movementQuery = defineQuery([Position, Velocity]);
+
+const movementSystem = defineSystem((world) => {
+    const ents = movementQuery(world);
+    for (let i = 0; i < ents.length; i++) {
+        const eid = ents[i];
+        Position.x[eid] += Velocity.x[eid];
+        Position.y[eid] += Velocity.y[eid];
+        Position.z[eid] += Velocity.z[eid];
+    }
+    return world;
+});
+
+const timeSystem = defineSystem((world) => {
+    const { time } = world;
+    const now = performance.now();
+    const delta = now - time.then;
+    time.delta = delta;
+    time.elapsed += delta;
+    time.then = now;
+
+    return world;
+});
+
+const pipeline = pipe(movementSystem, timeSystem);
+
+const world = createWorld();
+world.time = { delta: 0, elapsed: 0, then: performance.now() };
+
+const eid = addEntity(world);
+addComponent(world, Position, eid);
+addComponent(world, Velocity, eid);
+
+setInterval(() => {
+    pipeline(world);
+}, 16);
 
 //https://www.html5gamedevs.com/topic/42741-fast-pixi-tilemap-renderer-trying-to-fix/
 
